449e7c46de2b083505db7be99e8c986e2d4fa60c
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-06-03 01:33:52 -0300

    The Low-Res Update

--- Pong/main.lua
+++ Pong/main.lua
@@ -1,10 +1,18 @@
 --[[ 
-    Pong - 0
-    "The Day-0 Update"
+    Pong - 1
+    "The Low-Res Update"
 ]]
+
+-- push é uma biblioteca que nos permite desenhar nosso jogo
+-- em uma resolução mais virtual, fornecendo uma estética retrô
+-- https://github.com/Ulydev/push
+push = require 'push'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
 --[[
     Irá rodar quando o jogo 1 começar, apenas uma vez.
@@ -12,11 +20,29 @@ WINDOW_HEIGHT = 720
     inicial da execução do programa
 ]]  
 function love.load() 
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- evita o embaçamento do texto, deixando ele mais pixelado
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    
+    -- substitui a chamada love.window.setMode. 
+    -- essa nova chamada não leva em consideração a dimensão da janela real
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
+end
+
+--[[
+    Manipulação de teclado, chamada por LÖVE2D em cada quadro
+    love.keypressed() função de retorno de chamada que é executada 
+    sempre que pressionamos uma tecla
+]]
+
+function love.keypressed(key)
+    if key == 'escape' then
+        -- love.event.quit() finaliza a aplicação
+        love.event.quit()
+    end
 end
 
 --[[
@@ -30,10 +56,12 @@ end
     à direita ou ao centro na tela.
 ]]
 function love.draw()
-    love.graphics.printf(
-        'Oi pong!',
-        0,
-        WINDOW_HEIGHT / 2 -6,
-        WINDOW_WIDTH,
-        'center')
+    -- começa a renderização na resolução virtual
+    push:apply('start')
+
+    -- linha reorganizada (agora também usando uma altura e largura virtuais)
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- termina a renderização
+    push:apply('end')
 end
