a3a97520bbb05a130ae56523e8980c7a76360022
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-06-10 01:12:11 -0300

    The Class Update

--- Pong/main.lua
+++ Pong/main.lua
@@ -1,12 +1,25 @@
 --[[
-    Pong - 4
-    "The Ball Update"
+    Pong - 5
+    "The Class Update"
 ]]
 
 -- push é uma biblioteca que nos permite desenhar nosso jogo
 -- em uma resolução mais virtual, fornecendo uma estética retrô
 -- https://github.com/Ulydev/push
 push = require 'push'
+
+-- a biblioteca Class que usamos permite representar qualquer coisa do nosso
+-- jogo em forma de código, ao invés de acompanhar muitas variáveis e métodos
+-- diferentes
+Class = require 'class'
+
+-- nossa classe Paddle, que armazena posição e dimensão para cada barra
+-- e a lógica para renderiza-las
+require 'Paddle'
+
+-- nossa classe Ball, que não é muito diferente de uma estrutura de barras
+-- mas que funcionará de maneira muito diferente
+require 'Ball'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -32,9 +45,6 @@ function love.load()
     -- fonte que dá mais aspecto de retrô para qualquer texto
     smallFont = love.graphics.newFont('font.ttf', 8)
 
-    -- fonte maior para desenhar o placar
-    scoreFont = love.graphics.newFont('font.ttf', 32)
-
     -- define smallFont para todo objeto
     love.graphics.setFont(smallFont)
 
@@ -46,21 +56,13 @@ function love.load()
         vsync = true
     })
 
-  -- inicializa o placar e acompanha para mostrar o vencedor
-  player1Score = 0
-  player2Score = 0
+  -- inicializa as barras dos jogadores, faz delas globais, para servem
+  -- detectadas por outras funções e módulos
+  player1 = Paddle(10, 30, 5, 20)
+  player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
 
-  -- posições das barras no eixo Y (elas só podem subir ou descer)
-  player1Y = 30
-  player2Y = VIRTUAL_HEIGHT - 50
-
-  -- variáveis de velocidade e posição para a bola quando o jogo inicia
-  ballX = VIRTUAL_WIDTH / 2 - 2
-  ballY = VIRTUAL_HEIGHT / 2 - 2
-
-  -- função math.random retorna um valor aleatório entre o número da esquerda e da direita
-  ballDX = math.random(2) == 1 and 100 or -100
-  ballDY = math.random(-50, 50)
+  -- inicia nossa bola no centro da tela
+  ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
   -- variável de estado do jogo que é usada para fazer a transição entre diferentes
   -- partes do jogo (usada no início, menus, jogo principal etc)
@@ -75,33 +77,30 @@ end
 function love.update(dt)
   -- movimento do player 1
   if love.keyboard.isDown('w') then
-      -- adiciona velocidade negativa na barra ao Y atual dimensionado por DeltaTime (dt)
-      -- agora nós "apertamos" nossa posição entre os limites da tela
-      -- função math.max retorna o maior entre dois valores; 0 e o jogador Y
-      -- irá garantir que não iremos acima dele
-        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
+      player1.dy = -PADDLE_SPEED
   elseif love.keyboard.isDown('s') then
-      -- adiciona velocidade positiva na barra ao Y atual dimensionado por DeltaTime (dt)
-      -- math.min retorna o menor de dois valores; parte inferior da borda menos a altura da barra
-      -- também vai garantir que não iremos abaixo dele
-      player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+      player1.dy = PADDLE_SPEED
+  else
+      player1.dy = 0
   end
 
   -- movimento do player 2
   if love.keyboard.isDown('up') then
-      -- adiciona velocidade negativa na barra ao Y atual dimensionado por DeltaTime (dt)
-      player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
+      player2.dy = -PADDLE_SPEED
   elseif love.keyboard.isDown('down') then
-      -- adiciona velocidade positiva na barra ao Y atual dimensionado por DeltaTime (dt)
-      player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+      player2.dy = PADDLE_SPEED
+  else
+      player2.dy = 0
   end
 
   -- atualiza nossa bola baseada no DX e DY apenas se estivermos no estado de jogar
   -- dimensiona a velocidade por dt para que o movimento seja independentemente do framerate
   if gameState == 'play' then
-      ballX = ballX + ballDX * dt
-      ballY = ballY + ballDY * dt
+      ball:update(dt)
   end
+
+    player1:update(dt)
+    player2:update(dt)
 end
 
 --[[
@@ -121,13 +120,8 @@ function love.keypressed(key)
         else
             gameState = 'start'
 
-            -- inicia a bola no meio da tela
-            ballX = VIRTUAL_WIDTH / 2 - 2
-            ballY = VIRTUAL_HEIGHT / 2 - 2
-
-            -- da um valor inicial aleatório a velocidade x e y da bola
-            ballDX = math.random(2) == 1 and 100 or -100
-            ballDY = math.random(-50, 50) * 1.5
+            -- novo método de reset da bola
+            ball:reset()
         end
     end
 end
@@ -159,17 +153,12 @@ function love.draw()
         love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
     end
 
-    -- Aqui começaremos a desenhar as barras , que são retângulos na tela em certos pontos
-    -- assim como a bola
+    -- renderiza as barras, agora usando o método de sua classe
+    player1:render()
+    player2:render()
 
-    -- renderiza a primeira barra, do lado esquerdo
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
-
-    -- renderiza a segunda barra, lado direito
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
-
-    -- renderiza a bola no centro
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    -- renderiza a bola, agora usando o método de sua classe
+    ball:render()
 
     -- termina a renderização
     push:apply('end')
